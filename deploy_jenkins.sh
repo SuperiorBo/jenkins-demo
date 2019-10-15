bd1859f818ed68f0b91ff7aed811b05cd4dc1207
张双博 <shuangbo.zhang@zhiyoubao.com>
2019-10-15 19:06:03 +0800

    修复shell脚本项目路径问题

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -4,21 +4,22 @@ echo "0"
 CONTAINER="jenkins_demo"
 # 镜像名称（以日期时间为镜像标签，防止重复）
 IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
-echo "1"
 # 删除滚动更新残留的容
 docker rm `docker ps -a | grep -w $CONTAINER"_"$CONTAINER | awk '{print $1}'`
 # 强制删除滚动更新残留的镜像
 docker rmi --force `docker images | grep -w $IMAGE | awk '{print $3}'`
-echo "2"
+# 切换至项目路径
+cd $1
 # 创建新镜像
-docker build -t $IMAGE $1/. && \
-echo "3"
+docker build -t $IMAGE . && \
+# 构建完镜像切换至解决方案路径
+cd ..
 # 删除 docker-compose.jenkins.yml 文件，防止使用相同镜像
 rm -rf docker-compose.jenkins.yml && \
-echo "4"
+
 # 复制 docker-compose.src.yml 文件，防止污染原文件
 cp docker-compose.src.yml docker-compose.jenkins.yml && \
-echo "5"
+
 # 替换镜像名标志位为最新镜像
 sed -i s%IMAGE_LATEST%$IMAGE%g docker-compose.jenkins.yml && \
 
